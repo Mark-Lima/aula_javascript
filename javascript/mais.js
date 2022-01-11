a858c18e4aa67d03b8af5fd38835fd083c1e391e
Mark-Lima <marcklima1611@gmail.com>
2022-01-10 23:19:42 -0300

    Commit inicial
    Curso javaScript - Dio

---
+++ javascript/mais.js
@@ -0,0 +1,39 @@
+
+/*var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "banana", cor: "amarela"}];
+console.log (frutas [1].nome);
+
+var fruta = {nome: "maçã", cor: "vermelha"};
+console.log (fruta.nome);
+alert (fruta.cor);
+
+var lista = ["maça", "pêra", "laranja"];
+
+lista.push(); //insere um novo elemento na array
+
+lista.pop (); //remove o ultimo elemento da array
+
+console.log (lista.length);// informa a quantidade de elementos na array
+
+console.log (lista.reverse()); // retorna a ordem dos elementos invertidos dentro da array
+
+console.log (lista[0]) //retorna o primeiro elemento da array
+console.log (lista.toString()[0]) // retorna o primeiro elemento da string
+
+console.log (lista.join(" "))// transforma a arrey em string e separa os elementos pele caracter inserido entre as aspas (se não inserir valor nenhum entre as aspar o resultado é juntar todos os elementos da string)
+var nome = " Marcos Lima"; // Não é necessário declarar a variável como string
+
+var n1 = 36; // Não é necessário declarar a variável como integer 
+var n2 = 10;
+
+var frase = "Melhor enpadão é o de calabresa"
+alert (nome + " tem " + n1 + " anos"); // Cria um alerta na página
+
+alert (n1 + n2); // Soma (integer) ou concatena (string) dois valores
+console.log (nome); // Escreve no console
+console.log (n1 + n2); // Soma e retorna o resultado no console
+console.log (frase.replace ("calabresa", "4 Queijos"));// Substitui o valor do primeiro parametro pelo segundo informado
+
+alert(frase.replace ("calabresa", "4 Queijos")); // O mesmo comando qusado acima porém com retorno na janela de alerta.
+
+console.log (frase.toUpperCase ()); // Transforma todad as letras de uma string em maiusculas.
+console.log (frase.toLocaleLowerCase ());// transfoma todas as letras em uma string em minusculas*/
